b50de1b7386b5ce0c0562b5e01444214f226ab3d
Stephane Leclercq <leclercq@ekino.com>
2020-12-31 00:54:06 +0100

    feat(book) add simplest book class and list books from fritz2 example

---
+++ src/commonMain/kotlin/Book.kt
@@ -0,0 +1,6 @@
+/**
+ *
+ * Created on 12/29/20.
+ * @author Stephane Leclercq
+ */
+data class Book(val title: String)
--- src/jsMain/kotlin/app.kt
+++ src/jsMain/kotlin/app.kt
@@ -1,10 +1,46 @@
+import dev.fritz2.binding.RootStore
 import dev.fritz2.dom.html.render
 import dev.fritz2.dom.mount
 
+val books = listOf(
+    Book("Le prince et le pauvre"),
+    Book("Les quatre filles du docteur March")
+)
+
 fun main() {
+
+    val booksStore = object : RootStore<List<Book>>(books) {
+
+        var count = 0
+
+        val addItem = handle { list ->
+            count++
+            list + Book("yet another book title no. $count")
+        }
+
+        val deleteItem = handle<Book> { list, current ->
+            list.minus(current)
+        }
+
+    }
+
     render {
-        p {
-            +("Hello World!")
+        div("books") {
+            ul {
+                booksStore.data.renderEach { s ->
+                    li {
+                        button("btn", id = "delete-btn") {
+                            +s.title
+                            clicks.map { console.log("deleting $s"); s } handledBy booksStore.deleteItem
+                        }
+                    }
+                }
+            }
+            button("button") {
+                +"add an item"
+                clicks handledBy booksStore.addItem
+            }
         }
     }.mount("target")
+
 }
